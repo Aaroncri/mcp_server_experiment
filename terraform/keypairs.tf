bf51ffa3e84face47a1a3c34542090b6ba40ae18
Aaron Crighton <aaron.crighton@gmail.com>
2025-06-17 16:35:17 -0400

    typos

--- terraform/keypairs.tf
+++ terraform/keypairs.tf
@@ -12,5 +12,5 @@ on my local Linux box to create the key pair.
 resource "aws_key_pair" "mcp_project_key" {
   key_name   = "mcp_project_key"
   # Change this line:
-  public_key = file(var.public_key_path) # <-- Use the variable here
+  public_key = file(var.public_key_path) 
 }
